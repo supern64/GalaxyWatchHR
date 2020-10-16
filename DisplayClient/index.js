9c79ec472c2b7f5aa3a6897ba6d9b9d836fcbb0c
chanonlim <chanon-tong@hotmail.com>
2020-10-16 19:34:50 +0700

    add display client docs

--- DisplayClient/index.js
+++ DisplayClient/index.js
@@ -1,30 +1,32 @@
-var hr = document.getElementById("hr")
-var isConnected = false;
+const hr = document.getElementById("hr")
+let isConnected = false;
+
+const params = new URLSearchParams(window.location.search);
+const ip = params.get("ip") || "localhost";
 
 function connect() {
-	var socket = new WebSocket("ws://localhost:9288");
+	var socket = new WebSocket(`ws://${ip}:9288`);
 
 	socket.addEventListener('open', function()  {
 		socket.send(JSON.stringify({type: "handshake", role: "display"}));
 		isConnected = true;
 	});
-	socket.addEventListener('close', function()  {
-		hr.innerText = "0"
-		isConnected = false;
-	});
-	socket.addEventListener('error', function(evt)  {
-		hr.innerText = "0"
-		isConnected = false
-	});
+	socket.addEventListener('close', resetConnection);
 	socket.addEventListener('message', function (event) {
-	    var message = JSON.parse(event.data)
-	    hr.innerText = message.hr
+	    var message = JSON.parse(event.data);
+	    hr.innerText = message.hr;
 	});
+}
+
+function resetConnection() {
+	hr.innerText = "0";
+	isConnected = false;
 }
 
 setInterval(() => {
 	if (isConnected === false) {
-		connect()
+		connect();
 	}
-}, 10000)
-connect()
+}, 10000);
+
+connect();
